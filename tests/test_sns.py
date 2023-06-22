40cd901fb496c2ce3498fac8cdfb801500e65a28
Daniel Stahl <danstahl1138@gmail.com>
2023-06-22 05:51:23 -0500

    working test

--- tests/test_sns.py
+++ tests/test_sns.py
@@ -1,10 +1,21 @@
 from moto import mock_sns
+from moto.sns import sns_backends
 
 from prefect_aws.sns import SNS
 
 
-@mock_sns
+# @mock_sns
 def test_sns_publishes(aws_credentials):
-    task = SNS(aws_credentials=aws_credentials, sns_arn="some_test_arn")
-    task.publish("mysubject", "mymessage")
-    assert True
+    with mock_sns():
+        mock_topic = (
+            aws_credentials.get_boto3_session()
+            .client("sns")
+            .create_topic(Name="mock-topic")
+        )
+        topic_arn = mock_topic.get("TopicArn")
+        task = SNS(aws_credentials=aws_credentials, sns_arn=topic_arn)
+        task.publish("mysubject", "mymessage")
+        sns_backend = sns_backends["123456789012"]["us-east-1"]
+        all_send_notifications = sns_backend.topics[topic_arn].sent_notifications
+        assert all_send_notifications[0][1] == "mymessage"
+        assert all_send_notifications[0][2] == "mysubject"
